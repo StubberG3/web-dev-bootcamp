8ef5e9169558d550417c2ba2ed660742ee953c69
StubberG3 <stubberg3@gmail.com>
2021-05-12 21:21:27 -0400

    Return functions as values (factories)

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
-function callTwice (func) {
-    func();
-    func();
-}
+// function makeMysteryFunc () {
+//     const rand = Math.random();
+//     if (rand > 0.5) {
+//         return function () {
+//             console.log('CONGRATS, I AM A GOOD FUNCTION!\nYOU WIN A MILLION DOLLARS!!');
+//         }
+//     }
+//     else {
+//         return function () {
+//             alert('YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS');
+//         }
+//     }
+// }
 
-function callTenTimes (f) {
-    for (let i = 0; i < 10; i++) {
-        f();
+// const mystery = makeMysteryFunc();
+
+// mystery();
+
+function makeBetweenFunc (min, max) {
+    return function (num) {
+        return num >= min && num <= max;
     }
 }
 
-function rollDie () {
-    const roll = Math.floor(Math.random() * 6) + 1;
-    console.log(roll);
-}
+const isChild = makeBetweenFunc(0, 18);
+const isAdult = makeBetweenFunc(19, 64);
+const isSenior = makeBetweenFunc(65, 120);
 
-// callTwice(rollDie);
+isSenior(40);
 
-callTenTimes(rollDie);
+
+// function isBetween (num) {
+//     return num >= 50 && num <= 100;
+// }
+
+// function isBetween2 (num) {
+//     return num >= 1 && num <= 10;
+// }
