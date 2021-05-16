6425d8f05ff6d062cdb14a216a4353e5ef57f5b2
StubberG3 <stubberg3@gmail.com>
2021-05-16 01:10:56 -0400

    Practice reduce

--- js/app.js
+++ js/app.js
@@ -1,64 +1,50 @@
 /**
- * FILTER
- * creates a new array with all elements that pass the test
- * implemented by the provided function
- * create a subset of the original array
+ * REDUCE
+ * Executes a reducer function on each element of the array,
+ * resulting in a single value
+ * ways to boil things down: math, comparison, etc
  */
+
+const array1 = [1, 2, 3, 4];
 
 /**
- * MAP
- * The map() method creates a new array populated with the results of
- * calling a provided function on every element in the calling array.
+ *
+ * @param {*} accumulator - total, holds the sum
+ * @param {*} currentValue - each individual element
+ * @returns value used for next iteration's accumulator
  */
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
-/**
- * our callback returns true or false
- * if true, add n to the filtered array
- * @returns {Boolean}
- */
-const odds = nums.filter(n => {
-    return n % 2 === 1; // [9, 7, 5, 3, 1]
-})
+// 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer));
+// expected output: 10
 
-const smallNums = nums.filter(n => n < 5); // [4, 3, 2, 1]
+// 5 + 1 + 2 + 3 + 4
+console.log(array1.reduce(reducer, 5));
+// expected output: 15
 
-const anime = [
-    {
-        title: 'RE: Zero',
-        score: 90,
-        year: 2015
-    },
-    {
-        title: 'Evangelion',
-        score: 80,
-        year: 2000
-    },
-    {
-        title: 'Cowboy Bebop',
-        score: 70,
-        year: 1995
-    },
-    {
-        title: 'KON!',
-        score: 60,
-        year: 1980
-    },
-    {
-        title: 'Pokemon',
-        score: 50,
-        year: 1970
-    }
-]
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
 
-const goodAnime = anime.filter(a => a.score >= 80);
-// const goodTitles = goodAnime.map(a => a.title);
-const goodTitles = anime.filter(a => a.score >= 80)
-    .map(a => a.title);
-const badAnime = anime.filter(a => a.score < 80);
-const recentAnime = anime.filter(a => a.year >= 1990);
-console.table(anime);
-// console.table(goodAnime);
-// console.table(badAnime);
-// console.table(recentAnime);
-console.log(goodTitles);
+// const total = prices.reduce((total, price) => {
+//     return total + price;
+// });
+
+console.log(prices.reduce((total, price) => total + price));
+// expected output: 111.97
+
+// get most expensive price
+console.log(prices.reduce((min, price) => {
+    return price > min ? price : min;
+}));
+// expected output: 49.99
+
+// get cheapest price
+console.log(prices.reduce((min, price) => {
+    return price < min ? price : min;
+}));
+// expected output: 1.50
+
+const evens = [2, 4, 6, 8];
+console.log(evens.reduce((sum, num) => sum + num, 100));
+// expected output (0): 20
+// expected output (100): 120
