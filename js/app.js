ac7f8d3c8d914465d49adfdc97c07e7be88a5be4
StubberG3 <stubberg3@gmail.com>
2021-05-17 23:48:08 -0400

    Practice spread

--- js/app.js
+++ js/app.js
@@ -1,19 +1,37 @@
-// default params ES6
+// spread ES6
+// convert iterables into separate arguments
 
-// old way
-// function rollDie (numSides) {
-//     if (numSides === undefined) {
-//         numSides = 6;
-//     }
-//     return Math.floor(Math.random() * numSides) + 1;
-// }
+// const nums = [1, 2, 3, 4, 5];
 
-// new way
-function rollDie (numSides = 6) {
-    return Math.floor(Math.random() * numSides) + 1;
+// console.log(Math.max(...nums));
+
+// console.log(nums);
+// console.log(...nums);
+
+// console.log('hello');
+// console.log(...'hello'); // pass in as separate characters
+
+// spread array(s) into another array
+// const cats = ['Blue', 'Scout', 'Rocket'];
+// const dogs = ['Rusty', 'Wyatt'];
+
+// const allPets = [...cats, ...dogs, 'Hamilton'];
+
+// // spread iterables into an array
+
+// console.log([..."hello"]);
+
+// spread in object literals
+
+const feline = { legs: 4, family: 'Felidae' };
+const canine = { isFurry: true, family: 'Caninae' };
+
+const catDog = { ...feline, color: 'black', ...canine };
+
+const dataFromForm = {
+    email: 'testuser@gmail.com',
+    password: 'password1',
+    username: 'testuser'
 }
 
-// have default params come after non default params
-function greet (person, msg = "Hey there", punc = "!") {
-    console.log(`${msg}, ${person}${punc}`);
-}
+const mergedData = { ...dataFromForm, gender: 'M' };
